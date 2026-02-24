05cd6b13dc68fbe5a05d3710d983151e456a223e
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-02-23 17:43:58 -0800

    Add Traversal-quick-check. Finished both problems in 15 minutes.

--- src/QuickCheck.java
+++ src/QuickCheck.java
@@ -15,7 +15,19 @@ public class QuickCheck {
    * @param node the root node of the binary tree
    */
   public static void printLongerThan7(TreeNode<String> node) {
+    // if node is null, return
+    if (node == null ) return;
+    // if the length of the node.value is greater than 7
+    if (node.value.length() > 7) {
+      // print the value of the current node
+      System.out.println(node.value);
+    }  
+    // traverse down the left subtree
+    printLongerThan7(node.left);
+    // traverse down the right subtree
+    printLongerThan7(node.right);
   }
+
 
   /**
    * Returns the sum of all nodes holding odd numbers.
@@ -28,6 +40,23 @@ public class QuickCheck {
    * @return the sum of all odd-valued nodes
    */
   public static int oddSum(TreeNode<Integer> node) {
-      return -1;
+    // if node is null, return 0;
+    if (node == null) return 0;
+    // variable to hold the current nodes values (0 if even)
+    int current;
+    // if node.value modulo 2 is not 0 (odd)
+    if (node.value % 2 != 0) {
+      // current equals node.value
+      current = node.value;
+    } else {
+      // else current equals 0
+      current = 0;
+    }
+    // traverse left subtree and save it
+    int leftSum = oddSum(node.left);
+    // traverse right subtree and save it
+    int rightSum = oddSum(node.right);
+    // return left sum + right sum + current 
+    return leftSum + rightSum + current;
   }
 }
